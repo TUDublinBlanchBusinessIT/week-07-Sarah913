fb53c682de923439f65cb2fd0a91a86c29cfa757
Sarah Forde <you@example.com>
2026-03-16 16:48:39 +0000

    step 2 completed

---
+++ resources/views/layouts/navAuth.blade.php
@@ -0,0 +1,27 @@
+<ul class="navbar-nav text-white">
+    @if(Auth: :guest())
+    <li>
+        <a href="{{route('register')}}">Register
+            <span class="glyphicon glyphicon-pencil"></span>
+</a>
+
+</li>
+<li>
+    <a href="{{route('login')}}">Login
+        <span class="glyphicon glyphicon-log-in"></span>
+</a>
+</li>
+@else
+
+<li>
+    <a href="{{route('logout')}}">Logout
+        <span class="glyphicon glyphicon-log-out"></span>
+</a>
+</li>
+@endif
+</ul>
+
+
+
+
+    
